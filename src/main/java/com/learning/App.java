46ad18098563763403484cf54f170e5a65b05838
Adarsh-Kumar-Mishra <adarshmishra360@gmail.com>
2021-12-16 23:26:49 +0530

    basic annotation added

--- src/main/java/com/learning/App.java
+++ src/main/java/com/learning/App.java
@@ -1,5 +1,9 @@
 package com.learning;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,7 +15,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Project started..." );
         Configuration cfg = new Configuration();
@@ -23,11 +27,30 @@ public class App
         st.setName("Adarsh");
         st.setCity("New Delhi");
         System.out.println(st);
-        Session session =factory.openSession();
+        
+        //creating object of address class
+        Address ad = new Address();
+        ad.setStreet("street1");
+        ad.setCity("Delhi");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date() );
+        ad.setX(123.45);
+        
+        //reading image
+        FileInputStream fis = new FileInputStream("src/main/java/pic.png");
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
+        fis.close();
+        
+        
+       Session session =factory.openSession();
        Transaction tx = session.beginTransaction();
        session.save(st);
+       session.save(ad);
        tx.commit();
        session.close();
+       System.out.println("Done...");
     }
 }
 
